2da03afa5442299b166c1639440fc53e6ac381ac
Samarth Chugh <iforgot3360@gmail.com>
2026-09-22 20:14:15 +0530

    meak errors class

--- basic.py
+++ basic.py
@@ -1,6 +1,16 @@
 # CONSTANTS
 DIGITS = '0123456789'
 
+# Error
+class Error:
+    def __init__(self, error_name, details):
+        self.error_name=error_name
+        self.details=details
+
+    def as_string(self):
+        result=f'{self.error_name}: {self.details}'
+        return result
+
 #Tokens
 
 TT_INT = 'INT'
