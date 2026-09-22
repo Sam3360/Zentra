8a0fd1c5f62e520bef50877af67d71e56a9671da
Samarth Chugh <iforgot3360@gmail.com>
2026-09-22 20:09:00 +0530

    working on lexer

--- basic.py
+++ basic.py
@@ -1,3 +1,8 @@
+# CONSTANTS
+DIGITS = '0123456789'
+
+#Tokens
+
 TT_INT = 'INT'
 TT_FLOAT = 'FLOAT'
 TT_PLUS = 'PLUS'
@@ -7,6 +12,7 @@ TT_DIV = 'DIV'
 TT_LPAREN = 'LPAREN'
 TT_RPAREN = 'RPAREN'
 
+
 class Token:
     def __init__(self, type_, value):
         self.type=type_
@@ -14,4 +20,64 @@ class Token:
     def __repr__(self):
         if self.value: return f'{self.type}:{self.value}'
         return f'{self.type}'
-    
+
+
+# Lexer
+class Lexer:
+    def __init__(self, text):
+        self.text=text
+        self.pos=-1
+        self.current_char=None
+        self.advance()
+
+    def advance(self):
+        self.pos+=1
+        if self.pos<len(self.text):
+            self.current_char=self.text[self.pos]
+        else:
+            self.current_char=None
+
+    def make_tokens(self):
+        tokens=[]
+        while self.current_char!=None:
+            if self.current_char in ' \t':
+                self.advance()
+            elif self.current_char in '0123456789':
+                tokens.append(self.generate_number())
+            elif self.current_char=='+':
+                tokens.append(Token(TT_PLUS))
+                self.advance()
+            elif self.current_char=='-':
+                tokens.append(Token(TT_MINUS))
+                self.advance()
+            elif self.current_char=='*':
+                tokens.append(Token(TT_MUL))
+                self.advance()
+            elif self.current_char=='/':
+                tokens.append(Token(TT_DIV))
+                self.advance()
+            elif self.current_char=='(':
+                tokens.append(Token(TT_LPAREN))
+                self.advance()
+            elif self.current_char==')':
+                tokens.append(Token(TT_RPAREN))
+                self.advance()
+            else:
+                raise Exception(f"Illegal character '{self.current_char}'")
+        return tokens
+
+    def generate_number(self):
+        num_str=''
+        dot_count=0
+        while self.current_char!=None and (self.current_char in '0123456789.'):
+            if self.current_char=='.':
+                if dot_count==1: break
+                dot_count+=1
+                num_str+='.'
+            else:
+                num_str+=self.current_char
+            self.advance()
+        if dot_count==0:
+            return Token(TT_INT, int(num_str))
+        else:
+            return Token(TT_FLOAT, float(num_str))
